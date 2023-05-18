986f7e14c52f67bf4c17f38d5f03e7d04753f819
amirakame <amirakamel343@gmail.com>
2023-05-17 17:38:13 -0700

    chool

--- Interpreter_code.c
+++ Interpreter_code.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - A simple UNIX command line interpreter
  *
@@ -14,17 +13,14 @@ int main(void)
 	int n;
 
 	while (1)
+
 	{
 		printf("#cisfun$ ");
 		n = getline(&line, &len, stdin);
-
 		if (n <= 0)
 			exit(0);
-
 		line[n - 1] = '\0';
-
 		if (execve(line, (char **) &line, NULL) == -1)
-
 		{
 			printf("%s: No such file or directory\n", line);
 		}
